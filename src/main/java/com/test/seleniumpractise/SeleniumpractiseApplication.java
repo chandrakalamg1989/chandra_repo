ee7245e4e72184573eb03accd12442406f54d0ac
chandrakalamg1989 <chandrakalamgb@gmail.com>
2024-08-12 21:40:57 +0530

    Add files via upload

---
+++ src/main/java/com/test/seleniumpractise/SeleniumpractiseApplication.java
@@ -0,0 +1,13 @@
+package com.test.seleniumpractise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumpractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumpractiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/seleniumpractise/SeleniumpractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.seleniumpractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumpractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
